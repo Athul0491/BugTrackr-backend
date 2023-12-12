7e990f240adfc34eabd345c54636b53c52acd855
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-12-13 00:32:16 +0530

    feat: everything

---
+++ startup/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const config = require('config');
+module.exports = function () {
+  const db = config.get('db');
+  console.log(db);
+  mongoose
+    .connect(db)
+    .then(() => console.log(`Connected to ${db}...`))
+    .catch((err) => console.error(`${err} Could not connect to MongoDB....`));
+};
